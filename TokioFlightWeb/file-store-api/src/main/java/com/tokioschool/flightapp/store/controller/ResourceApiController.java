55184de9821dc319fa439c4e4415115952202240
andres.rpenuela <andresruizpenuela@gmail.com>
2024-12-12 18:06:41 +0100

    [PRJ0003] se crea el controlador de tipo rest, se añade el manejador de excepcioens y se crea un punto de arranque para spring

---
+++ TokioFlightWeb/file-store-api/src/main/java/com/tokioschool/flightapp/store/controller/ResourceApiController.java
@@ -0,0 +1,35 @@
+package com.tokioschool.flightapp.store.controller;
+
+import com.tokioschool.flightapp.store.core.exception.NotFoundException;
+import com.tokioschool.flightapp.store.dto.ResourceContentDto;
+import com.tokioschool.flightapp.store.service.StoreService;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/store/api/resources")
+@Validated // de spring
+public class ResourceApiController {
+
+    private final StoreService storeService;
+
+    @GetMapping("/{resourceId}")
+    public ResponseEntity<ResourceContentDto> getResourceHandler(
+            @NotNull @PathVariable UUID resourceId) {
+
+        ResourceContentDto resourceContentDto = storeService.findResource(resourceId)
+                .orElseThrow(()-> new NotFoundException("Resource with id: %s not found!.".formatted(resourceId)));
+
+        return ResponseEntity.ok(resourceContentDto);
+    }
+
+}
