4048c5838a6b30b34b8f9eec5a17f5dadb30f2c5
andres.rpenuela <andresruizpenuela@gmail.com>
2024-12-12 20:17:55 +0100

    [PRJ0004] consumir un servicio de store, mediante con rest template

--- TokioFlightWeb/flight-web/src/main/java/com/tokioschool/flightapp/flight/store/impl/StoreFacadeRestTemplateImpl.java
+++ TokioFlightWeb/flight-web/src/main/java/com/tokioschool/flightapp/flight/store/impl/StoreFacadeRestTemplateImpl.java
@@ -4,15 +4,22 @@ import com.tokioschool.flightapp.flight.store.StoreFacade;
 import com.tokioschool.flightapp.flight.store.dto.ResourceContentDto;
 import com.tokioschool.flightapp.flight.store.dto.ResourceIdDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Deprecated
 @Service("storeFacadeRestTemplate")
 @RequiredArgsConstructor
-public class StoreFacadeImpl implements StoreFacade {
+@Slf4j
+public class StoreFacadeRestTemplateImpl implements StoreFacade {
+
+    private final RestTemplate restTemplate;
+
     @Override
     public Optional<ResourceIdDto> saveResource(MultipartFile multipartFile, String description) {
         return Optional.empty();
@@ -20,6 +27,17 @@ public class StoreFacadeImpl implements StoreFacade {
 
     @Override
     public Optional<ResourceContentDto> findResource(UUID resourceId) {
+        final String url = "http://loclahost:8081/sotre/api/resources/{resourceId}";
+        try{
+            ResourceContentDto resourceContentDto = restTemplate.getForObject(url,
+                    ResourceContentDto.class,
+                    resourceId);
+
+            return Optional.ofNullable(resourceContentDto);
+        }catch (Exception e){
+            log.error("Exception in findResource",e);
+        }
+
         return Optional.empty();
     }
 
