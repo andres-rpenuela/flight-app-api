796dd148bd22e0eae100e86699de802a5a93d72e
andres.rpenuela <andresruizpenuela@gmail.com>
2024-12-14 15:54:54 +0100

    [PRJ0007] refactor code

--- TokioFlightWeb/file-store-api/src/main/java/com/tokioschool/flightapp/store/scurity/configuration/filter/StoreApiSecurityConfiguration.java
+++ TokioFlightWeb/file-store-api/src/main/java/com/tokioschool/flightapp/store/scurity/configuration/filter/StoreApiSecurityConfiguration.java
@@ -1,4 +1,4 @@
-package com.tokioschool.flightapp.store.scurity;
+package com.tokioschool.flightapp.store.scurity.configuration.filter;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
