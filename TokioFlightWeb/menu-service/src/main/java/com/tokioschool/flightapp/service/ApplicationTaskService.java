7d039304b15f386a572b56ab721f355932049e34
andres.rpenuela <andresruizpenuela@gmail.com>
2024-12-26 14:25:01 +0100

    [PRJ0013] created scheme of application runner

---
+++ TokioFlightWeb/menu-service/src/main/java/com/tokioschool/flightapp/service/ApplicationTaskService.java
@@ -0,0 +1,23 @@
+package com.tokioschool.flightapp.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * Tarea que se ejecuta cuando inicia la aplicaiocn de menu service, y
+ * cuando finaliza, se cierra la aplicación, es la aplicaicon
+ * mas basica de spring boot
+ */
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class ApplicationTaskService implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+            log.info("ApplicationTaskService started");
+    }
+}
