f541736a5231da17dcc0e7b6a3bd7efd4f8b58fc
andres.rpenuela <andresruizpenuela@gmail.com>
2024-12-26 21:23:45 +0100

    [PRJ0013] add repository

---
+++ TokioFlightWeb/menu-service/src/main/java/com/tokioschool/flightapp/repository/MenuDao.java
@@ -0,0 +1,9 @@
+package com.tokioschool.flightapp.repository;
+
+import com.tokioschool.flightapp.domain.Menu;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository // optional el uso de la anotacion
+public interface MenuDao extends MongoRepository<Menu,String> {
+}
