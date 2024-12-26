053185730ff15eb9affffebe85f414595fc4fa2e
andres.rpenuela <andresruizpenuela@gmail.com>
2024-12-26 20:59:19 +0100

    [PRJ0013] add configuration db

---
+++ TokioFlightWeb/menu-service/src/main/java/com/tokioschool/flightapp/config/MongoDbConfiguration.java
@@ -0,0 +1,9 @@
+package com.tokioschool.flightapp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@Configuration
+@EnableMongoRepositories
+public class MongoDbConfiguration {
+}
