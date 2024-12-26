344d39bdeed307b8df50cbd4ad409037467d4347
andres.rpenuela <andresruizpenuela@gmail.com>
2024-12-26 21:35:57 +0100

    [PRJ0013] added the package to scanner where hosting the repositories

--- TokioFlightWeb/menu-service/src/main/java/com/tokioschool/flightapp/config/MongoDbConfiguration.java
+++ TokioFlightWeb/menu-service/src/main/java/com/tokioschool/flightapp/config/MongoDbConfiguration.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
-@EnableMongoRepositories
+@EnableMongoRepositories(basePackages = "com.tokioschool.flightapp.repository")
 public class MongoDbConfiguration {
 }
