7a18e3d10695c4c5994fe57ffcdc34ccb2c8d883
andres.rpenuela <andresruizpenuela@gmail.com>
2024-12-27 10:47:16 +0100

    [PRJ0013] count menus in collection, and if there aren't, created a new collection of random menu, when run application

--- TokioFlightWeb/menu-service/src/main/java/com/tokioschool/flightapp/runner/ApplicationTaskService.java
+++ TokioFlightWeb/menu-service/src/main/java/com/tokioschool/flightapp/runner/ApplicationTaskService.java
@@ -1,10 +1,15 @@
 package com.tokioschool.flightapp.runner;
 
+import com.tokioschool.flightapp.domain.Menu;
+import com.tokioschool.flightapp.repository.MenuDao;
+import com.tokioschool.flightapp.service.MenuService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.Collection;
 
 /**
  * Tarea que se ejecuta cuando inicia la aplicaiocn de menu service, y
@@ -16,8 +21,24 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class ApplicationTaskService implements ApplicationRunner {
 
+    private final MenuService menuService;
+    private final MenuDao menuDao;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
             log.info("ApplicationTaskService started");
+
+            // comprueba si hay menu creado o no
+            long countMenus = menuDao.count();
+            log.info("Count menus: {}",countMenus);
+
+            if(countMenus == 0){
+                Collection<Menu> randomMenus = menuService.createRandomMenus();
+                Menu menu10 = menuDao.findById(randomMenus
+                        .toArray(new Menu[0])[10]
+                        .getId()
+                ).orElseGet(()->null);
+                log.info("Menu 10 random: {}",menu10);
+            }
     }
 }
